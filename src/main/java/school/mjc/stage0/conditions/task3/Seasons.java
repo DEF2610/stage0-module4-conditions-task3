dd450421328ee93679f0faf90466ef801745cf6e
Temka <maksimukart@gmail.com>
2023-01-29 18:38:53 +0100

    d

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -5,11 +5,11 @@ public class Seasons {
         if (month > 12 || month < 0)
             System.out.println("wrong number!");
         else {
-            if (month >= 3 && month <= 5)
+            if (month > 2 && month < 6)
                 System.out.println("Spring");
-            if (month >= 5 && month <= 8)
+            else if (month > 5 && month < 9)
                 System.out.println("Summer");
-            if (month >= 8 && month <= 10)
+            else if (month > 8 && month < 12)
                 System.out.println("Fall");
             else
                 System.out.println("Winter");
