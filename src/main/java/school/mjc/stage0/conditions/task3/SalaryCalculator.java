84c5acabb28d6ab4f5732474a7ff3dc6e21f604b
Temka <maksimukart@gmail.com>
2023-01-29 18:42:17 +0100

    d

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -4,9 +4,9 @@ public class SalaryCalculator {
     public void calculateSalary(int salary) {
         if (salary > 0 && salary <= 10000)
             System.out.println(salary-(salary*15/100.0));
-        if (salary >10000 && salary <= 20000)
+        else if (salary >10000 && salary <= 20000)
             System.out.println(salary-(salary*18/100.0));
-        if (salary > 20000)
+        else if (salary > 20000)
             System.out.println(salary-(salary*20/100.0));
         else
             System.out.println("wrong input!");
