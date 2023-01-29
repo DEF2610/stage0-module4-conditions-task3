c45d20ab007f53fc19acb1190bf77d2acae4e1dc
Temka <maksimukart@gmail.com>
2023-01-29 16:39:49 +0100

    d

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+        System.out.println("I don't know");
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-
+        if (month > 12 || month < 0)
+            System.out.println("wrong number!");
+        else {
+            if (month >= 3 && month <= 5)
+                System.out.println("Spring");
+            if (month >= 5 && month <= 8)
+                System.out.println("Summer");
+            if (month >= 8 && month <= 10)
+                System.out.println("Fall");
+            else
+                System.out.println("Winter");
+        }
     }
 }
